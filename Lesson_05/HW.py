df19be026d5f7ee9c0d8c12106a2765dce17f97a
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-01-28 17:17:36 +0100

    Changed cickle

--- Lesson_05/HW.py
+++ Lesson_05/HW.py
@@ -9,7 +9,7 @@ with open(
     search_string = input("Please enter a search word: ")
 
     lines = file_content.split()
-    found_in_lines = [line for line in lines if search_string in line]
+    found_in_lines = [line for line in lines if search_string == line]
 
     if found_in_lines:
         print(
