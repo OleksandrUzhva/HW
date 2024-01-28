662ca3f7573024caa55a9656a6ff33129e07b5fe
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-01-28 16:20:22 +0100

    added changes in input text

--- Lesson_05/HW.py
+++ Lesson_05/HW.py
@@ -6,7 +6,7 @@ with open(
 ) as file:
     file_content = file.read()
 
-    search_string = input("Введіть стоку для пошука: ")
+    search_string = input("Введіть строку для пошука: ")
 
     lines = file_content.split("\n")
     found_in_lines = [line for line in lines if search_string in line]
