4b5f317b1a1fa5a0d9433796911b020a5aaa8cec
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-01-31 13:27:40 +0100

    Added changes

--- Lesson_05/HW.py
+++ Lesson_05/HW.py
@@ -1,4 +1,7 @@
+import time
 from pathlib import Path
+
+start_time = time.time()
 
 current_path = Path(__file__).parent
 with open(
@@ -12,9 +15,11 @@ with open(
     found_in_lines = [line for line in lines if search_string == line]
 
     if found_in_lines:
-        print(
-            f"Words'{search_string}' found in  ({len(found_in_lines)} шт.):"
-        )
+        print(f"Words'{search_string}' found in  ({len(found_in_lines)} ps.):")
 
     else:
         print(f"words '{search_string}' weren`t found.")
+
+stop_time = time.time()
+operating_time = stop_time - start_time
+print(f"Operating time: {operating_time} sec")
