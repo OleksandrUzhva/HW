bb9c5befa2d5fe514544870e582367d51da749a8
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-02-14 21:06:36 +0100

    HW_10

--- Lesson_10/HW_10.py
+++ Lesson_10/HW_10.py
@@ -1,9 +1,11 @@
+import datetime
+import json
 from dataclasses import dataclass
 
 import requests
 
 middle_currency = "CHF"
-API_KEY = "0KBIDSI0Y6YSK58E"
+API_KEY = "D5G28HSZ0FWYD5WQ"
 
 
 @dataclass
@@ -46,7 +48,25 @@ def convert_to(value: float, currency_from: str, currency_to: str) -> float:
     response: requests.Response = requests.get(
         f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency_from}&to_currency={currency_to}&apikey={API_KEY}"
     )
+
     result: dict = response.json()
+    with open("log.json", "a") as log:
+        time_now = datetime.datetime.now()
+        time: str = time_now.strftime("%d/%m/%y %H:%M")
+        log_data = {
+            "currency_from": result["Realtime Currency Exchange Rate"][
+                "1. From_Currency Code"
+            ],
+            "currency_to": result["Realtime Currency Exchange Rate"][
+                "3. To_Currency Code"
+            ],
+            "rate": result["Realtime Currency Exchange Rate"]["5. Exchange Rate"],
+            "timestamp": time,
+        }
+
+        json.dump(log_data, log, indent=2)
+        log.write("\n")
+
     coefficient: float = float(
         result["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
     )
@@ -57,3 +77,17 @@ flight = Price(value=200, currency="USD")
 hotel = Price(value=350, currency="GBP")
 total = flight + hotel
 print(total)
+
+# {
+#     "Realtime Currency Exchange Rate": {
+#         "1. From_Currency Code": "USD",
+#         "2. From_Currency Name": "United States Dollar",
+#         "3. To_Currency Code": "JPY",
+#         "4. To_Currency Name": "Japanese Yen",
+#         "5. Exchange Rate": "150.72200000",
+#         "6. Last Refreshed": "2024-02-14 14:04:02",
+#         "7. Time Zone": "UTC",
+#         "8. Bid Price": "150.71710000",
+#         "9. Ask Price": "150.72900000"
+#     }
+# }
